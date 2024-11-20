6765d0a4909f73f2a66eed59f991a4c3bd8c4ec8
Shivamchaudhary03 <vatss0127@gmail.com>
2024-11-21 00:25:01 +0530

    Update AirlineInfoDao.java

--- dao/AirlineInfoDao.java
+++ dao/AirlineInfoDao.java
@@ -3,7 +3,7 @@ package com.airline.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.dhanush.airline.entity.AirlineInfo;
+import com.airline.entity.AirlineInfo;
 
 @Repository
 public interface AirlineInfoDao extends JpaRepository<AirlineInfo, Long>{
