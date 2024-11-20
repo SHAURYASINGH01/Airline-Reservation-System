efd4243e4a6797d9c701ead0f265ed2856878255
Shivamchaudhary03 <vatss0127@gmail.com>
2024-11-21 00:26:23 +0530

    Update FlightDao.java

--- dao/FlightDao.java
+++ dao/FlightDao.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.dhanush.airline.entity.Flight;
+import com.airline.entity.Flight;
 
 @Repository
 public interface FlightDao extends JpaRepository<Flight, Long> {
